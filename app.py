98b428f26e2c233b8378bda78b64e9cf99f7b9f7
jowharfazal <44577727+jowharfazal@users.noreply.github.com>
2021-05-30 23:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-""" Brokerage Calculator for Zerodha, Upstox, PayTM & Tradeplus """
+""" Trade P/L Calculator for Zerodha/Upstox/Tradeplus/PayTM """
 
 import streamlit as st
-st.set_page_config(page_title="Trade P/L calculator",page_icon='rupees_symbol_1.ico')
+
+st.set_page_config(page_title="Trade P/L calculator",page_icon='rupees_symbol_1.ico') #':chart:')
 
 GST = 18
-
-RIGHT_ALIGN_TAG = "text-align:right;"
-LEFT_ALIGN_TAG = "text-align:left;"
-CENTER_ALIGN_TAG = "text-align:center;"
-
 
 def calc_charges(broker, instrtype, instrkey,rates,buy,sell,qty):
     buyamt = buy*qty
@@ -45,12 +41,11 @@ def calc_charges(broker, instrtype, instrkey,rates,buy,sell,qty):
     # pl = (sellamt-buyamt-totalcharges
 
 
-    return [brokerage,stt,exchtrnchrg,clearingchrg,gst,sebicharg,stampduty]
+    return [brokerage,stt,exchtrnchrg+clearingchrg,gst,sebicharg,stampduty]
 
-    
 
 instrtypes = ['EQ Intraday', 'EQ Delivery', 'Futures', 'Options']
-instrtypesdict = {'EQ Intraday':0, 'EQ Delivery':1, 'Futures':2, 'Options':3}
+instrtypesdict = {'Intraday':0, 'Delivery':1, 'Futures':2, 'Options':3}
 brokers = ["Zerodha", "UpStox", "TradePlus", "PayTM"]
 rates = { "Zerodha":    { "Brokerage": [40,0,40,40], 
                           "STT": [0.025,0.1,0.01,0.05],
@@ -67,9 +62,9 @@ rates = { "Zerodha":    { "Brokerage": [40,0,40,40],
                           "StampDuty": [0.003,0.015,0.002,0.003],
                         },
            "TradePlus": { "Brokerage": [18,0,18,0], 
-                          "STT": [0.025,0.1,0.01,0.05],
+                          "STT": [0.025,0.1,0.1,0.05],
                           "ExchTrnChrg": [0.00345,0.00345,0.002,0.053],
-                          "ClearingChrg": [0,0,0,0],
+                          "ClearingChrg": [0,0,0.016,0.025],
                           "SEBIChrg": [0.0001,0.0001,0.0001,0.0001],
                           "StampDuty": [0.003,0.015,0.002,0.003],
                         },
@@ -83,19 +78,14 @@ rates = { "Zerodha":    { "Brokerage": [40,0,40,40],
         }
          
 
-st.title("Trade P&L Calculator")
+st.title("Trade P/L Calculator")
+
+broker = st.radio("Broker", brokers, index=1)
+instrtype = st.radio("Type of Instrument", instrtypes, index=3)
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 inp,dummy,outp1,sep,outp2,dummy1,dummy2 = st.beta_columns((1.7,0.2,1.5,0.1,1,2.2,2.2))
 
-#broker = inp.radio("Broker", brokers, index=1)
-broker = inp.selectbox("Broker", brokers,index=0)
-
-#instrtype = inp.radio("Type of Instrument", instrtypes, index=3)
-instrtype = inp.selectbox("Type of Instrument", instrtypes, index=3)
-
-# buy = inp.number_input("Buy",value=0.0,step=0.05)
-# sell = inp.number_input("Sell",value=0.0,step=0.05)
-# qty = inp.number_input("Qty",value=0,step=1)
 buy = inp.text_input("Buy",value=0)
 sell = inp.text_input("Sell",value=0)
 qty = inp.text_input("Qty",value=0)
@@ -118,38 +108,30 @@ charges = calc_charges(broker, instrtype, instrtypesdict[instrtype],rates,buy,se
 totcharges = round(sum(charges),2)
 pl = round((sell-buy)*qty-totcharges,2)
 
-outp1.write('Brokerage')
-outp1.write("STT")
-outp1.write("Exch.Trn. Chrg")
-outp1.write("Clearing Chrg")
-outp1.write("GST")
-outp1.write("SEBI Chrg")
-outp1.write("Stamp Duty")
-#outp1.markdown("""~~~""")
+outp1.write('<p style="font-size:85%">Brokerage</p>', unsafe_allow_html=True)
+outp1.write('<p style="font-size:85%">STT</p>', unsafe_allow_html=True)
+outp1.write('<p style="font-size:85%">Exch+Clearing</p>', unsafe_allow_html=True)
+outp1.write('<p style="font-size:85%">GST</p>', unsafe_allow_html=True)
+outp1.write('<p style="font-size:85%">SEBI Chrg</p>', unsafe_allow_html=True)
+outp1.write('<p style="font-size:85%">Stamp Duty</p>', unsafe_allow_html=True)
 
-sep.write(': ')
-sep.write(": ")
-sep.write(": ")
-sep.write(": ")
-sep.write(": ")
-sep.write(": ")
-sep.write(": ")
-#sep.markdown("""---""")
+sep.write('<p style="font-size:85%">:</p>', unsafe_allow_html=True)
+sep.write('<p style="font-size:85%">:</p>', unsafe_allow_html=True)
+sep.write('<p style="font-size:85%">:</p>', unsafe_allow_html=True)
+sep.write('<p style="font-size:85%">:</p>', unsafe_allow_html=True)
+sep.write('<p style="font-size:85%">:</p>', unsafe_allow_html=True)
+sep.write('<p style="font-size:85%">:</p>', unsafe_allow_html=True)
 
 for charge in charges:
-    out_html = '<p style="text-align:right;">'+str(charge)+'</p>'
+    out_html = '<p style="text-align:right;font-size:85%">'+str(charge)+'</p>'
     outp2.write(out_html, unsafe_allow_html=True)
-#outp2.markdown("""---""")
 
-outp1.write('<p style="font-size:110%"><b>Tot Charges</b></p>', unsafe_allow_html=True)
-outp1.markdown("""---""")
+outp1.write('<p style="font-size:100%"><b>Tot Charges</b></p>', unsafe_allow_html=True)
 sep.write(': ')
-sep.markdown("""---""")
 
 totcharges_html = '<p style="color:Blue;text-align:right;"><b>'+f"{totcharges:,.2f}"+'</b></p>'
 
 outp2.write(totcharges_html, unsafe_allow_html=True)
-outp2.markdown("""---""")
 outp1.write('<p style="font-size:115%"><b>P/L</b></p>', unsafe_allow_html=True)
 if pl>0:
     pl_html = '<p style="text-align:right;color:green;font-size:115%"><b>'+f"{pl:,.2f}"+'</b></p>'
@@ -158,8 +140,5 @@ elif pl<0:
 else:
     pl_html = '<p style="text-align:right><b>'+f"{pl:,.2f}"+'</b></p>'
 
-outp2.write(pl_html, unsafe_allow_html=True)
 sep.write(': ')
-# sep.markdown("""---""")
-# outp1.markdown("""---""")
-# outp2.markdown("""---""")
+outp2.write(pl_html, unsafe_allow_html=True)
