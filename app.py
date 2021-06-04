79ae067d9e123ee89ec9dd4d81b70ff9931a8778
jowharfazal <44577727+jowharfazal@users.noreply.github.com>
2021-06-04 23:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,14 +129,14 @@ for charge in charges:
 outp1.write('<p style="font-size:100%"><b>Tot Charges</b></p>', unsafe_allow_html=True)
 sep.write(': ')
 
-totcharges_html = '<p style="color:Blue;text-align:right;"><b>'+f"{totcharges:,.2f}"+'</b></p>'
+totcharges_html = '<p style="color:Black;text-align:right;"><b>'+f"{totcharges:,.2f}"+'</b></p>'
 
 outp2.write(totcharges_html, unsafe_allow_html=True)
 outp1.write('<p style="font-size:115%"><b>P/L</b></p>', unsafe_allow_html=True)
 if pl>0:
-    pl_html = '<p style="text-align:right;color:LightGreen;font-size:115%;"><b>'+f"{pl:,.2f}"+'</b></p>'
+    pl_html = '<p style="text-align:right;color:DarkBlue;font-size:115%;"><b>'+f"{pl:,.2f}"+'</b></p>'
 elif pl<0:
-    pl_html = '<p style="text-align:right;color:DarkRed;font-size:115%;"><b>'+f"{pl:,.2f}"+'</b></p>'
+    pl_html = '<p style="text-align:right;color:DarkBlue;font-size:115%;"><b>'+f"{pl:,.2f}"+'</b></p>'
 else:
     pl_html = '<p style="text-align:right><b>'+f"{pl:,.2f}"+'</b></p>'
 
